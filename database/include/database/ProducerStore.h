0eda1e518194923243b111d09dbcd8fae34c5bbe
guoyonggang <hero.gariker@gmail.com>
2018-08-16 14:11:38 +0800

    Add Producer Store

---
+++ database/include/database/ProducerStore.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 GSENetwork
+ *
+ * This file is part of GSENetwork.
+ *
+ * GSENetwork is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ */
+
+#pragma once
+
+#include "database/Database.h"
+
+namespace database {
+
+class ProducerStore : public Database {
+public:
+    ProducerStore();
+
+    ~ProducerStore();
+
+private:
+
+};
+
+} // end of namespace database
