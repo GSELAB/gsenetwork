29d5d0169c027bb9c41b09077ff1d23648811a2a
guoyonggang <hero.gariker@gmail.com>
2018-08-16 15:01:29 +0800

    Add Producer Bundle

---
+++ bundle/ProducerBundle.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 GSENetwork
+ *
+ * This file is part of GSENetwork.
+ *
+ * GSENetwork is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ */
+
+#include "bundle/ProducerBundle.h"
+
+namespace bundle {
+
+
+ProducerBundle::ProducerBundle(char *data)
+{
+
+}
+
+ProducerBundle::ProducerBundle(pchain::Producer &producer)
+{
+
+}
+
+ProducerBundle::ProducerBundle(ProducerBundle &producerBundle)
+{
+
+}
+
+char *ProducerBundle::getData()
+{
+
+}
+
+} // end of namespace bundle
