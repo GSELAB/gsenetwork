1693c8f5a9bfa2f207c13d289009083377774bf0
guoyonggang <hero.gariker@gmail.com>
2018-08-18 17:18:16 +0800

    Add Constant Argument

---
+++ config/include/config/Constant.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 GSENetwork
+ *
+ * This file is part of GSENetwork.
+ *
+ * GSENetwork is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ */
+
+#pragma once
+
+//
+#define MAX_TRANSACTION_QUEUE_SIZE  (1024 * 128)
+
+#define MAX_BLOCK_QUEUE_SIZE (1024 * 2)
+
+// Transaction size 1 KB()
+#define MAX_TRANSACTION_SIZE 1024
+
+// Max block size 1024 KB (1 M)
+#define MAX_BLOCK_SIZE (1024 * 1024 * 1204)
+
