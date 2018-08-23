a4bff70ca4bf43f2615bf75fb30ab959ca89a4ef
guoyonggang <hero.gariker@gmail.com>
2018-08-23 19:38:19 +0800

    Add Test unit main content

--- programs/gsenode/main.cpp
+++ programs/gsenode/main.cpp
@@ -11,16 +11,25 @@
 
 #include <iostream>
 
-void printGSELogo()
-{
-    std::cout << "" << std::endl;
-}
+#include <boost/version.hpp>
+#include <boost/timer.hpp>
+#include "chain/DbManager.h"
+#include "chain/SubChainController.h"
+#include "chain/Controller.h"
 
+using namespace std;
 
 int main(int argc, char **argv)
 {
+    boost::timer t;
+    cout << "max timespan: " << t.elapsed_max() / 3600 << "h" << endl;
+    cout << "min timespan: " << t.elapsed_min() << "s" << endl;
+    cout << "now time elapsed: " << t.elapsed() << "s" << endl;
+    cout << "boost version" << BOOST_VERSION <<endl;
+    cout << "boost lib version" << BOOST_LIB_VERSION <<endl;
+    std::cout << "GSENetwork Interview!" << std::endl;
 
-    std::cout << "GSENetwork protocol!" << std::endl;
+    //std::cout << "GSENetwork protocol!" << std::endl;
     return 0;
 }
 
