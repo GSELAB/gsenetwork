966377527a8f1a4dc4e546d4432ea7eb7ee3c386
guoyonggang <hero.gariker@gmail.com>
2018-08-24 17:48:45 +0800

    Add Chain ID using

---
+++ chain/include/chain/Types.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 GSENetwork
+ *
+ * This file is part of GSENetwork.
+ *
+ * GSENetwork is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+namespace chain {
+
+using ChainId = uint64_t;
+
+}
