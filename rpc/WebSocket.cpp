bab4e827bd864be165b7d2757f8e236a2f725651
hero.gariker@gmail.com <zaqwedc@120911>
2018-09-26 18:27:23 +0800

    Add rpc content

--- rpc/WebSocket.cpp
+++ rpc/WebSocket.cpp
@@ -3,7 +3,24 @@
 
 namespace rpc {
 
-WebSocket::WebSocket()
+//using websocketpp::lib::bind;
+
+void on_message(RpcServer* server, ConnectHDL hdl, MessagePtr msg)
+{
+    //socket
+}
+
+void on_http(RpcServer* server, ConnectHDL hdl)
+{
+
+}
+
+void on_socket_init(ConnectHDL hdl, boost::asio::ip::tcp::socket& s)
+{
+
+}
+
+WebSocket::WebSocket(unsigned short listenPort): m_listenPort(listenPort)
 {
 
 }
@@ -13,8 +30,43 @@ WebSocket::~WebSocket()
 
 }
 
-bool WebSocket::start()
+bool WebSocket::socketInit()
 {
+    try {
+        m_rpcServer.clear_access_channels(websocketpp::log::alevel::all);
+        m_rpcServer.clear_error_channels(websocketpp::log::alevel::all);
+
+        m_rpcServer.init_asio();
+        m_rpcServer.set_reuse_addr(true);
+
+        m_rpcServer.set_message_handler(websocketpp::lib::bind(&on_message, &m_rpcServer, websocketpp::lib::placeholders::_1, websocketpp::lib::placeholders::_2));
+        m_rpcServer.set_socket_init_handler(websocketpp::lib::bind(&on_socket_init, websocketpp::lib::placeholders::_1, websocketpp::lib::placeholders::_2));
+        m_rpcServer.set_http_handler(websocketpp::lib::bind(&on_http, &m_rpcServer, websocketpp::lib::placeholders::_1));
+
+        m_rpcServer.set_listen_backlog(8190);
+        m_rpcServer.listen(m_listenPort);
+        m_rpcServer.start_accept();
+
+        //??? should start multi threads
+        if (1) {
+            m_rpcServer.run();
+        } else {
+            /*
+            typedef websocketpp::lib::shared_ptr<websocketpp::lib::thread> thread_ptr;
+
+            std::vector<thread_ptr> ts;
+            for (size_t i = 0; i < num_threads; i++) {
+                ts.push_back(websocketpp::lib::make_shared<websocketpp::lib::thread>(&server::run, &m_rpcServer));
+            }
+
+            for (size_t i = 0; i < num_threads; i++) {
+                ts[i]->join();
+            }
+            */
+        }
+    } catch(websocketpp::exception const & e) {
+        throw e;
+    }
 
     return true;
 }
--- rpc/include/rpc/WebSocket.h
+++ rpc/include/rpc/WebSocket.h
@@ -4,15 +4,12 @@
 
 #include <websocketpp/config/core.hpp>
 #include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
 
 namespace rpc {
 
 namespace ba = boost::asio;
 namespace bi = boost::asio::ip;
-
-typedef websocketpp::server<websocketpp::config::core> RpcServer;
-typedef websocketpp::connection_hdl ConnectHDL;
-typedef RpcServer::message_ptr MessagePtr;
 
 class WebSocketEventHandlerFace {
 public:
@@ -20,19 +17,61 @@ public:
 
 };
 
+struct AsioStubConfig: public websocketpp::config::asio {
+    // pull default settings from our core config
+    typedef websocketpp::config::asio core;
+    typedef core::concurrency_type concurrency_type;
+    typedef core::request_type request_type;
+    typedef core::response_type response_type;
+    typedef core::message_type message_type;
+    typedef core::con_msg_manager_type con_msg_manager_type;
+    typedef core::endpoint_msg_manager_type endpoint_msg_manager_type;
+    typedef core::alog_type alog_type;
+    typedef core::elog_type elog_type;
+    typedef core::rng_type rng_type;
+    typedef core::endpoint_base endpoint_base;
+
+    static bool const enable_multithreading = true;
+
+    struct transport_config : public core::transport_config {
+        typedef core::concurrency_type concurrency_type;
+        typedef core::elog_type elog_type;
+        typedef core::alog_type alog_type;
+        typedef core::request_type request_type;
+        typedef core::response_type response_type;
+        static bool const enable_multithreading = true;
+    };
+
+    typedef websocketpp::transport::asio::endpoint<transport_config>        transport_type;
+
+    static const websocketpp::log::level elog_level =        websocketpp::log::elevel::all;
+    static const websocketpp::log::level alog_level =        websocketpp::log::alevel::all;
+};
+
+//typedef websocketpp::server<websocketpp::config::core> RpcServer;
+typedef websocketpp::server<AsioStubConfig> RpcServer;
+typedef websocketpp::connection_hdl ConnectHDL;
+typedef RpcServer::message_ptr MessagePtr;
+
 class WebSocket {
 public:
-    WebSocket();
+    WebSocket(unsigned short listenPort);
 
     ~WebSocket();
 
-    bool start();
+    bool socketInit();
 
     bool shutdown();
 
     void onMessage(ConnectHDL hdl, MessagePtr msg);
 
+    void onHttp(RpcServer* server, ConnectHDL hdl);
+
+    void send();
+
 private:
+    unsigned short m_listenPort;
     RpcServer m_rpcServer;
+    // boost::optional<bi::tcp::endpoint> m_httpListenEndpoint;
 };
 }
