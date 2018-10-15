7a23eeefd42c943f9e2b3bd334cbefd020fb8436
hero.gariker@gmail.com <zaqwedc@120911>
2018-10-15 11:36:12 +0800

    Add Schedule interface

--- producer/Schedule.cpp
+++ producer/Schedule.cpp
@@ -10,4 +10,56 @@ ProducerScheduleType& ProducerScheduleType::operator=(ProducerScheduleType const
     return *this;
 }
 
+bool ProducerScheduleType::isExist(Address const& address)
+{
+    auto itr = std::find(m_producers.begin(), m_producers.end(), address);
+    if (itr != m_producers.end()) return true;
+    return false;
+}
+
+// ------------------------
+Schedule::Schedule()
+{
+
+}
+
+Schedule::~Schedule()
+{
+
+}
+
+std::vector<Producer> const& Schedule::getActiceProducers() const
+{
+    std::vector<Producer> ret;
+    Guard l(x_activeProducers);
+    for (auto& i : m_activeProducers)
+        ret.emplace_back(i);
+    return ret;
+}
+
+void Schedule::schedule()
+{
+
+}
+
+void Schedule::addActiveProducer(Producer const& producer)
+{
+
+}
+
+void Schedule::addActiveProducer(Address const& address)
+{
+
+}
+
+void Schedule::removeActiveProducer(Producer const& producer)
+{
+
+}
+
+void Schedule::removeActiveProducer(Address const& producer)
+{
+
+}
+
 }
--- producer/include/producer/Schedule.h
+++ producer/include/producer/Schedule.h
@@ -3,6 +3,8 @@
 #include <vector>
 
 #include <core/Address.h>
+#include <core/Guards.h>
+#include <core/Producer.h>
 
 using namespace core;
 
@@ -16,8 +18,33 @@ public:
 
     size_t size() const { return m_producers.size(); }
 
+    bool isExist(Address const& address);
+
 public:
     std::vector<Address> m_producers;
+};
+
+class Schedule {
+public:
+    Schedule();
+
+    ~Schedule();
+
+    std::vector<Producer> const& getActiceProducers() const { Guard l(x_activeProducers); return m_activeProducers; }
+
+    void schedule();
+
+    void addActiveProducer(Producer const& producer);
+
+    void addActiveProducer(Address const& address);
+
+    void removeActiveProducer(Producer const& producer);
+
+    void removeActiveProducer(Address const& producer);
+
+private:
+    mutable Mutex x_activeProducers;
+    std::vector<Producer> m_activeProducers;   //
 };
 
 }
