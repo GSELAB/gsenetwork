479bfbdd93c982ee105c9c1ecff12adb7253bd5e
hero.gariker@gmail.com <zaqwedc@120911>
2018-10-16 16:40:28 +0800

    Add HeaderConfirmation Valid Check

--- chain/include/chain/BlockState.h
+++ chain/include/chain/BlockState.h
@@ -7,18 +7,19 @@ using namespace core;
 
 namespace chain {
 
-#define HeaderConfirmationFields 5
+#define HEADER_CONFIRMATION_FIELDS_WITHOUT_SIG (5)
+#define HEADER_CONFIRMATION_FIELDS_ALL (HEADER_CONFIRMATION_FIELDS_WITHOUT_SIG + 1)
 
 class HeaderConfirmation: public core::Object {
 public:
-    HeaderConfirmation(ChainID chainID, uint64_t number, core::BlockID blockID, core::Address producer):
-        m_chainID(chainID), m_number(number), m_blockID(blockID), m_producer(producer) {}
+    HeaderConfirmation(ChainID chainID, uint64_t number, core::BlockID blockID, int64_t timestamp, core::Address producer):
+        m_chainID(chainID), m_number(number), m_blockID(blockID), m_timestamp(timestamp), m_producer(producer) {}
 
     HeaderConfirmation(core::bytesConstRef data);
 
     HeaderConfirmation(HeaderConfirmation const& confirmation):
         m_chainID(confirmation.getChainID()), m_number(confirmation.getNumber()),
-        m_blockID(confirmation.getBlockID()), m_producer(confirmation.getProducer()), m_signature(confirmation.getSignature()) {}
+        m_blockID(confirmation.getBlockID()), m_timestamp(getTimestamp()), m_producer(confirmation.getProducer()), m_signature(confirmation.getSignature()) {}
 
     HeaderConfirmation& operator=(HeaderConfirmation const& confirmation);
 
@@ -28,6 +29,8 @@ public:
 
     void streamRLP(core::RLPStream& rlpStream) const;
 
+    void streamRLPContent(core::RLPStream& rlpStream) const;
+
     void sign(Secret const& privKey);
 
     ChainID getChainID() const { return m_chainID; }
@@ -36,9 +39,11 @@ public:
 
     core::BlockID const& getBlockID() const { return m_blockID; }
 
+    int64_t getTimestamp() const { return m_timestamp; }
+
     core::Address const& getProducer() const { return m_producer; }
 
-    crypto::SignatureStruct const getSignature() const { return m_signature; }
+    crypto::SignatureStruct const& getSignature() const { return m_signature; }
 
     core::h256 getHash();
 
@@ -55,6 +60,7 @@ private:
     ChainID m_chainID;
     uint64_t m_number;
     core::BlockID m_blockID;
+    int64_t m_timestamp;
     core::Address m_producer;
     crypto::SignatureStruct m_signature;
 
--- crypto/include/crypto/Valid.h
+++ crypto/include/crypto/Valid.h
@@ -2,12 +2,18 @@
 
 #include <crypto/Common.h>
 #include <core/Transaction.h>
-
+#include <core/Block.h>
+#include <chain/BlockState.h>
 
 using namespace core;
+using namespace chain;
 
 namespace crypto {
 
-bool isValidSig(Transaction const& transaction);
+bool isValidSig(Transaction& transaction);
+
+bool isValidSig(Block& block);
+
+bool isValidSig(HeaderConfirmation& headerConfirmation);
 
 }
