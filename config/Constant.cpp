b17220a111ca4e55817ce7564819ae2e11eee455
hero.gariker@gmail.com <zaqwedc@120911>
2018-11-07 13:18:34 +0800

    Move AttributeState location

--- config/Constant.cpp
+++ config/Constant.cpp
@@ -11,15 +11,7 @@
 
 #include <config/Constant.h>
 
-using namespace core;
 
 namespace config {
-
-AttributeState<bool> ATTRIBUTE_GENESIS_INITED(toBytes(ATTRIBUTE_GENESIS_INITED_KEY), true);
-
-AttributeState<uint64_t> ATTRIBUTE_CURRENT_BLOCK_HEIGHT(toBytes(ATTRIBUTE_CURRENT_BLOCK_HEIGHT_KEY), (uint64_t)0);
-
-AttributeState<bytes> ATTRIBUTE_PREV_PRODUCER_LIST(toBytes(ATTRIBUTE_PREV_PRODUCER_LIST_KEY), EmptyBytes);
-AttributeState<bytes> ATTRIBUTE_CURRENT_PRODUCER_LIST(toBytes(ATTRIBUTE_CURRENT_PRODUCER_LIST_KEY), EmptyBytes);
 
 } // end namespace
---
+++ core/AttributeState.cpp
@@ -0,0 +1,68 @@
+#include <core/AttributeState.h>
+#include <core/Log.h>
+
+namespace core {
+
+AttributeState ATTRIBUTE_GENESIS_INITED(toBytes(ATTRIBUTE_GENESIS_INITED_KEY), 1);
+
+AttributeState ATTRIBUTE_CURRENT_BLOCK_HEIGHT(toBytes(ATTRIBUTE_CURRENT_BLOCK_HEIGHT_KEY), (uint64_t)0);
+
+AttributeState ATTRIBUTE_PREV_PRODUCER_LIST(toBytes(ATTRIBUTE_PREV_PRODUCER_LIST_KEY));
+
+AttributeState ATTRIBUTE_CURRENT_PRODUCER_LIST(toBytes(ATTRIBUTE_CURRENT_PRODUCER_LIST_KEY));
+
+AttributeState::AttributeState(bytes const& key, bytesConstRef data): m_key(key)
+{
+    try {
+        core::RLP rlp(data);
+        if (rlp.isList() && rlp.itemCount() == 2) {
+            m_type = (Type)rlp[0].toInt<uint32_t>();
+            if (m_type == Uint64Type) {
+                m_value = (uint64_t)rlp[1].toInt<uint64_t>();
+            } else if (m_type == BytesType) {
+                m_data = rlp[2].data().toBytes();
+            }
+        } else {
+            throw AttributeStateException("None list || None CONSTANT_STATE_FIELDS");
+        }
+    } catch (AttributeStateException& e) {
+        throw e;
+    } catch (Exception& e) {
+        throw e;
+    }
+}
+
+AttributeState& AttributeState::operator=(AttributeState const& as)
+{
+    if (this == &as) return *this;
+    m_key = as.m_key;
+    m_type = as.m_type;
+    if (m_type == Uint64Type) {
+        m_value = as.m_value;
+    } else if (m_type == BytesType) {
+        m_data = as.m_data;
+    }
+    return *this;
+}
+
+void AttributeState::streamRLP(RLPStream& rlpStream) const
+{
+    rlpStream.appendList(2);
+    rlpStream << m_type;
+    if (m_type == Uint64Type) {
+        rlpStream << m_value;
+    } else if (m_type == BytesType) {
+        rlpStream.appendRaw(m_data);
+    }
+}
+
+bytes AttributeState::getRLPData()
+{
+    RLPStream rlpStream;
+    streamRLP(rlpStream);
+    CINFO << "AttributeState::getRLPData size:" << rlpStream.out().size();
+    return rlpStream.out();
+}
+
+
+}
