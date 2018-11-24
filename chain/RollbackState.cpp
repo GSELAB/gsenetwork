9ce8be48512d4bced471be1753272cddb364146f
hero.gariker@gmail.com <zaqwedc@120911>
2018-11-24 16:10:38 +0800

    Repair segment error bugs due to risk condition

--- chain/RollbackState.cpp
+++ chain/RollbackState.cpp
@@ -18,12 +18,14 @@ RollbackState::~RollbackState()
 
 void RollbackState::close()
 {
+    Guard l{x_index};
     if (m_index.size() == 0) return;
     m_index.clear();
 }
 
 BlockStatePtr RollbackState::getBlock(BlockID const& blockID) const
 {
+    Guard l{x_index};
     auto itr = m_index.find(blockID);
     if (itr != m_index.end()) return *itr;
     return EmptyBlockStatePtr;
@@ -31,6 +33,7 @@ BlockStatePtr RollbackState::getBlock(BlockID const& blockID) const
 
 BlockStatePtr RollbackState::getBlock(uint64_t number) const
 {
+    Guard l{x_index};
     auto const& numberIdx = m_index.get<ByBlockNumber>();
     auto itr = numberIdx.find(number);
     if (itr != numberIdx.end())
@@ -40,9 +43,12 @@ BlockStatePtr RollbackState::getBlock(uint64_t number) const
 
 void RollbackState::set(BlockStatePtr bsp)
 {
-    auto ret = m_index.insert(bsp);
-    if (!ret.second) {
-        throw RollbackStateException("Duplicate state!");
+    {
+        Guard l{x_index};
+        auto ret = m_index.insert(bsp);
+        if (!ret.second) {
+            throw RollbackStateException("Duplicate state!");
+        }
     }
 
     if (!m_head) {
@@ -58,16 +64,19 @@ BlockStatePtr RollbackState::add(Block& block, ProducerSnapshot const& ps, bool 
         throw RollbackStateException("Not set head block!");
     }
 
-    auto& item = m_index.get<ByBlockID>();
-    auto existing = item.find(block.getHash());
-    if (existing != item.end()) {
-        throw RollbackStateException("Block has exist!");
-    }
+    {
+        Guard l{x_index};
+        auto& item = m_index.get<ByBlockID>();
+        auto existing = item.find(block.getHash());
+        if (existing != item.end()) {
+            throw RollbackStateException("Block has exist!");
+        }
 
-    auto parent = item.find(block.getBlockHeader().getParentHash());
-    if (parent == item.end()) {
-        CERROR << "Unlink block, id:" << block.getBlockHeader().getParentHash();
-        throw RollbackStateException("Unlink block, id!");
+        auto parent = item.find(block.getBlockHeader().getParentHash());
+        if (parent == item.end()) {
+            CERROR << "Unlink block, id:" << block.getBlockHeader().getParentHash();
+            throw RollbackStateException("Unlink block, id!");
+        }
     }
 
     auto bsp = std::make_shared<BlockState>(block);
@@ -81,15 +90,26 @@ BlockStatePtr RollbackState::add(Block& block, ProducerSnapshot const& ps, bool 
 
 BlockStatePtr RollbackState::add(BlockStatePtr nextBSP)
 {
-    auto ret = m_index.insert(nextBSP);
-    if (!ret.second) {
-        throw RollbackStateException("Duplicate block state add!");
+    uint64_t solidifyNumber;
+    BlockStatePtr oldest;
+    {
+        Guard l{x_index};
+        auto ret = m_index.insert(nextBSP);
+        if (!ret.second) {
+            throw RollbackStateException("Duplicate block state add!");
+        }
+
+        m_head = *m_index.get<ByBlockNumber>().begin();
+        auto _head = *m_index.get<ByMultiBlockNumber>().begin();
+        if (!m_head || !_head) {
+            CWARN << "Add block state failed due to bad header or multi header";
+            throw RollbackStateException("Add block state failed due to bad header or multi header");
+        }
+
+        solidifyNumber = _head->m_bftSolidifyBlockNumber;
+        oldest = *m_index.get<ByUpBlockNumber>().begin();
     }
 
-    m_head = *m_index.get<ByBlockNumber>().begin();
-    auto _head = *m_index.get<ByMultiBlockNumber>().begin();
-    uint64_t solidifyNumber = _head->m_bftSolidifyBlockNumber;
-    BlockStatePtr oldest = *m_index.get<ByUpBlockNumber>().begin();
     if (nextBSP->m_block.isSyncBlock()) {
         CWARN << "Process sync block(number:" << nextBSP->m_blockNumber
               << " tx-size:"
@@ -103,9 +123,16 @@ BlockStatePtr RollbackState::add(BlockStatePtr nextBSP)
               << ") to rollback state - current solidify number:"
               << solidifyNumber;
     }
+
     if (oldest->m_blockNumber < solidifyNumber) {
         auto solidifyBSP = getBlock(solidifyNumber);
-        solidifiable(solidifyBSP);
+        if (!solidifyBSP) {
+            CWARN << "Error occur when to silidify block:"
+                  << solidifyNumber
+                  << ", the block state could not be found.";
+            return nextBSP;
+        }
+        m_solidifiable(solidifyBSP);
     }
 
     return nextBSP;
@@ -113,31 +140,49 @@ BlockStatePtr RollbackState::add(BlockStatePtr nextBSP)
 
 void RollbackState::remove(BlockID const& blockID)
 {
+
     auto itr = m_index.find(blockID);
-    if (itr == m_index.end())
+    if (itr == m_index.end()) {
         return;
+    }
 
     auto prevBlockID = (*itr)->getPrev();
     auto prevItr = m_index.find(prevBlockID);
-    if (prevItr != m_index.end())
+    if (prevItr != m_index.end()) {
         remove(prevBlockID);
+    }
 
-    m_index.erase(itr);
+    {
+        Guard l{x_index};
+        m_index.erase(itr);
+    }
 }
 
 void RollbackState::remove(uint64_t number)
 {
-    auto& item = m_index.get<ByBlockNumber>();
-    auto itr = item.find(number);
-    if (itr == item.end()) return;
-    remove((*itr)->m_blockID);
+    bool needDel = false;
+    BlockID blockID;
+    {
+        Guard l{x_index};
+        auto& item = m_index.get<ByBlockNumber>();
+        auto itr = item.find(number);
+        if (itr != item.end()) {
+            blockID = (*itr)->m_blockID;
+            needDel = true;
+        }
+    }
+
+    if (needDel) {
+        remove(blockID);
+    }
 }
 
 void RollbackState::add(HeaderConfirmation const& confirmation)
 {
     BlockStatePtr bsp = getBlock(confirmation.getBlockID());
-    if (!bsp)
+    if (!bsp) {
         return;
+    }
 
     bsp->addConfirmation(confirmation);
     if (bsp->m_bftSolidifyBlockNumber < bsp->m_blockNumber && bsp->getConfirmationsSize() >= ((bsp->m_activeProucers.size() * 2) / 3)) {
@@ -168,7 +213,7 @@ std::pair<BranchType, BranchType> RollbackState::fetchBranchFrom(BlockID const& 
     std::pair<BranchType, BranchType> result;
     BlockStatePtr firstItem = getBlock(first);
     BlockStatePtr secondItem = getBlock(second);
-    while (firstItem->m_blockNumber > secondItem->m_blockNumber) {
+    while (firstItem && secondItem && firstItem->m_blockNumber > secondItem->m_blockNumber) {
         result.first.push_back(firstItem);
         BlockID prevBlockID = firstItem->getPrev();
         firstItem = getBlock(prevBlockID);
@@ -179,7 +224,7 @@ std::pair<BranchType, BranchType> RollbackState::fetchBranchFrom(BlockID const& 
 
     }
 
-    while (secondItem->m_blockNumber > firstItem->m_blockNumber) {
+    while (secondItem && firstItem && secondItem->m_blockNumber > firstItem->m_blockNumber) {
         result.second.push_back(secondItem);
         BlockID prevBlockID = secondItem->getPrev();
         secondItem = getBlock(prevBlockID);
@@ -189,7 +234,7 @@ std::pair<BranchType, BranchType> RollbackState::fetchBranchFrom(BlockID const& 
         }
     }
 
-    while (firstItem->getPrev() != secondItem->getPrev()) {
+    while (firstItem && secondItem && firstItem->getPrev() != secondItem->getPrev()) {
         result.first.push_back(firstItem);
         result.second.push_back(secondItem);
         firstItem = getBlock(firstItem->getPrev());
@@ -203,18 +248,15 @@ std::pair<BranchType, BranchType> RollbackState::fetchBranchFrom(BlockID const& 
     return result;
 }
 
-void RollbackState::solidifiable(BlockStatePtr const& bsp)
-{
-    auto _bsp = m_index.find(bsp->m_blockID);
-    if (_bsp != m_index.end()) {
-        m_solidifiable(*_bsp);
-    }
-}
-
 void RollbackState::setBFTSolidify(BlockID blockID)
 {
+    Guard l{x_index};
     auto& idx = m_index.get<ByBlockID>();
     auto itr = idx.find(blockID);
+    if (itr == idx.end()) {
+        return;
+    }
+
     uint64_t number = (*itr)->m_blockNumber;
     idx.modify(itr, [&](auto& bsp) {
         bsp->m_bftSolidifyBlockNumber = bsp->m_blockNumber;
